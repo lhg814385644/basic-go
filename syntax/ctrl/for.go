3455c28848a1e5e98b0caca9424900d367062f99
hg <haigguol@gmail.com>
2024-03-19 22:33:27 +0800

    for demo

--- syntax/ctrl/for.go
+++ syntax/ctrl/for.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func Loop1() {
 	for i := 0; i < 10; i++ {
 		println(i)
@@ -16,5 +18,15 @@ func Loop2() {
 	for i < 10 {
 		println(i)
 		i++
+	}
+}
+
+func Cal() {
+	for i := 10; i < 100; i++ {
+		for j := 10; j < 100; j++ {
+			if i+j == 80 && i-j == 20 {
+				fmt.Printf("i=%d,j=%d\n", i, j)
+			}
+		}
 	}
 }
