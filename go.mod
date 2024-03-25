e8a9a258c5360567fc79c4e97567b6dfdf7d071a
hg <haigguol@gmail.com>
2024-03-25 23:06:35 +0800

    跨域解决

--- go.mod
+++ go.mod
@@ -5,8 +5,10 @@ go 1.22.0
 require (
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/dlclark/regexp2 v1.11.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/gin-gonic/contrib v0.0.0-20221130124618-7e01895a63f2 // indirect
 	github.com/gin-gonic/gin v1.9.1 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
--- webook/main.go
+++ webook/main.go
@@ -1,1 +1,36 @@
-package webook
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/lhg814385644/basic-go/webook/internal/web"
+	"strings"
+	"time"
+)
+
+func main() {
+	hdl := web.NewUserHandler()
+	server := gin.Default()
+	server.Use(cors.New(cors.Config{
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://localhost") {
+				return true
+			}
+			return strings.Contains(origin, "your_company.com")
+		},
+		AllowedHeaders:   []string{"Content-Type"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+	// 自定义中间件
+	server.Use(logMid)
+	hdl.RegisterRoutes(server)
+
+	server.Run(":8080")
+}
+
+// 日志中间件
+func logMid(ctx *gin.Context) {
+	fmt.Printf("LogMid: %s\n", ctx.Request.Method)
+}
